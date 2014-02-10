65e534fe99391edc0d86d883a315d278a88fd7e8
nadjieh <ajafari@cern.ch>
2014-02-10 14:44:32 +0330

    Bug-fixed MassPlotter.cc & adding run_MassPlotsDoubleMu.C

---
+++ MT2Analysis/Code/MT2AnalysisCode/RootMacros/run_MassPlotsDoubleMu.C
@@ -0,0 +1,112 @@
+{
+  TString outputdir = "../MassPlots/";
+  TString samples   = "./samples/samplesMineDoubleMu.dat"; Int_t channel = 26; //2* 13 (Muon PDG Id)
+
+  int verbose =3;
+
+  gSystem->CompileMacro("../MT2Code/src/MassPlotter.cc", "kf");//"k", "f"
+  gROOT->ProcessLine(".x SetStyle_PRD.C");
+
+  int gNMT2bins                   = 17;
+  double  gMT2bins[gNMT2bins+1]   = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 150, 200, 300, 400, 550, 800}; 	
+  
+  int gNMT2Bbins                   = 15;
+  double  gMT2Bbins[gNMT2bins+1]   = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 150, 200, 300, 500}; 	
+
+  int gNMT2bins_l                   = 14;
+  double  gMT2bins_l[gNMT2bins+1]   = {0, 10, 20, 30, 40, 50, 65, 80, 95, 115, 140, 180, 250, 350, 500}; 	
+
+  // Change the name for output accordingly
+  MassPlotter *tA = new MassPlotter(outputdir, "MassPlots_DoubleMu.root");
+
+  tA->SetSave(false);
+  tA->setVerbose(verbose);
+  tA->init(samples);
+  tA->SetIsPhoton(false);
+
+  /*
+   * Define preselections including trigger
+   */
+
+  std::ostringstream triggerStream;
+  triggerStream << "( "
+		<<" misc.ProcessID != 0 || ((misc.CrazyHCAL==0 && misc.NegativeJEC==0 " <<"&&"
+		<<" misc.CSCTightHaloIDFlag==0 && misc.HBHENoiseFlag==0 " <<"&&"
+		<<" misc.hcalLaserEventFlag==0 && misc.trackingFailureFlag==0 " <<"&&"
+		<<" misc.eeBadScFlag==0 && misc.EcalDeadCellTriggerPrimitiveFlag==0 )" <<"&&("
+        << "(trigger.HLT_DiMuons) " << ")))"; //Channel specific trigger
+    	
+
+  TString trigger = triggerStream.str().c_str();
+
+  // We define the cut vector for cutflow table here to avoid double-coding
+  // The first element is preselection
+
+  std::vector<std::string> myChannelCuts;
+  myChannelCuts.push_back(std::string(trigger));
+
+
+  /*
+   * Define selection cuts and fill the cutflow input vector
+   */
+
+
+  // You need to specify the channel
+  TString myChan = "doubleMu";
+
+  // You need to carefully define the cut variables based on MT2"Channel".hh
+
+  myChannelCuts.push_back(std::string(std::string(myChan) + ".mu0Ind  != -1")); // First lepton index, channel specific
+  myChannelCuts.push_back(std::string(std::string(myChan) + ".mu1Ind  != -1")); // Second lepton index, channel specific
+  myChannelCuts.push_back(std::string(std::string(myChan) + ".chargeSum == 0"));
+  myChannelCuts.push_back(std::string(std::string(myChan) + ".hasNoVetoElec"));
+  myChannelCuts.push_back(std::string(std::string(myChan) + ".hasNoVetoMu"));
+  myChannelCuts.push_back("0 == 0");											//Place holder for Jet requirements
+  myChannelCuts.push_back("0 == 0");											//Place holder for MET requirements
+
+
+  // We need to make the cut stream
+
+  std::ostringstream cutStream;
+  cutStream << " ";
+  for(unsigned int iCut = 1; iCut < myChannelCuts.size(); iCut++){
+	cutStream << myChannelCuts[iCut];
+	if(iCut < (myChannelCuts.size() - 1))
+		cutStream	<<" && ";
+  }
+
+  TString cuts = cutStream.str().c_str();
+           
+
+
+    
+  /*
+   * Plot the high level variables: add what you like to see ....
+   */
+
+  TString mt2 = myChan + ".MT2";
+  TString metimb = myChan + ".METImbalanced";
+  TString mt2imb = myChan + ".MT2Imbalanced";
+  TString metimbPhi = myChan + ".METImbalancedPhi";
+
+  //MT2
+  tA->makePlot(mt2, cuts, -10, -10 , -10, trigger, mt2, 100, -2, 1000, false, true, true, true, true, true, 1, 1);     
+
+  //METImbalanced
+  tA->makePlot(metimb, cuts, -10, -10 , -10, trigger, metimb, 100, -2, 1000, false, true, true, true, true, true, 1, 1);     
+
+  //MT2Imbalanced
+  tA->makePlot(mt2imb, cuts, -10, -10 , -10, trigger, mt2imb, 100, -2, 1000, false, true, true, true, true, true, 1, 1);    
+
+  //MT2ImbalancedPhi
+  tA->makePlot(metimbPhi, cuts, -10, -10 , -10, trigger, metimbPhi, 70, -3.5, 3.5, false, true , true, true, true, true, 1, 1); 
+
+
+
+  /*
+   * Show cutflow table
+   */
+
+  tA->MakeCutFlowTable( myChannelCuts );
+
+}
