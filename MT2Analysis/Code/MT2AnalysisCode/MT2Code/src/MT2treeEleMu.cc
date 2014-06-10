bae062be80c9b070dc2c8870dff0c238683d7be1
shchenarani <shchenarani@github.com>
2014-06-10 13:58:12 +0430

    Pvisible_dot_Zeta to src/MT2treeEleMu.cc is added.

--- MT2Analysis/Code/MT2AnalysisCode/MT2Code/src/MT2treeEleMu.cc
+++ MT2Analysis/Code/MT2AnalysisCode/MT2Code/src/MT2treeEleMu.cc
@@ -1,8 +1,9 @@
 #include "MT2tree.hh"
 #include <iostream>
 #include <math.h>
-using namespace std;
 #include "helper/Utilities.hh"
+using namespace std;
+
 void MT2tree::FillEleMu(){
   //{-1,-1} is Default value or no good muon is found or more than two muon is found 
 
@@ -39,7 +40,7 @@ void MT2tree::FillEleMu(){
 
 
   
-    if(fVerbose > 3){ 
+      if(fVerbose > 3){ 
       cout<<" Indices for Ele Muon1 "<<eleMu[0].mu0Ind<<" "<<eleMu[0].ele0Ind<<endl;
       cout<<" eleMu[0].charge ele&& muon1"<<muo[eleMu[0].mu0Ind].Charge <<" "<<ele[eleMu[0].ele0Ind].Charge<<endl;
       cout<<"eleMu[0].charge1"<<  eleMu[0].charge<<endl;
@@ -51,12 +52,31 @@ void MT2tree::FillEleMu(){
     }  
     eleMu[0].DPhi =(fabs(Util::DeltaPhi(muo[eleMu[0].mu0Ind].lv.Phi(), ele[eleMu[0].ele0Ind].lv.Phi())));
     eleMu[0].MT2    = CalcMT2(0, 0,muo[eleMu[0].mu0Ind].lv,ele[eleMu[0].ele0Ind].lv, pfmet[0]);
-    
     eleMu[0].lv = muo[eleMu[0].mu0Ind].lv +ele[eleMu[0].ele0Ind].lv;
-    if(fVerbose > 3) cout<<" met.Pt() "<<eleMu[0].lv.Pt()<<endl;
+
+     float PvisibleX = ele[eleMu[0].ele0Ind].lv.Px() + muo[eleMu[0].mu0Ind].lv.Px();
+     float PvisibleY = ele[eleMu[0].ele0Ind].lv.Py() + muo[eleMu[0].mu0Ind].lv.Py();
+     float PvisibleZ = ele[eleMu[0].ele0Ind].lv.Pz() + muo[eleMu[0].mu0Ind].lv.Pz();
+ 
+     float ZetaX= (1/sqrt(2))* (ele[eleMu[0].ele0Ind].lv.Px() /sqrt(ele[eleMu[0].ele0Ind].lv.Px()*ele[eleMu[0].ele0Ind].lv.Px()+ele[eleMu[0].ele0Ind].lv.Py()*ele[eleMu[0].ele0Ind].lv.Py()+ele[eleMu[0].ele0Ind].lv.Pz()*ele[eleMu[0].ele0Ind].lv.Pz()) + muo[eleMu[0].mu0Ind].lv.Px()/ sqrt(muo[eleMu[0].mu0Ind].lv.Px()*muo[eleMu[0].mu0Ind].lv.Px()+muo[eleMu[0].mu0Ind].lv.Py()*muo[eleMu[0].mu0Ind].lv.Py()+ muo[eleMu[0].mu0Ind].lv.Pz()*muo[eleMu[0].mu0Ind].lv.Pz() ));
+       
+     float ZetaY=(1/sqrt(2))* (ele[eleMu[0].ele0Ind].lv.Py() /sqrt(ele[eleMu[0].ele0Ind].lv.Px()*ele[eleMu[0].ele0Ind].lv.Px()+ele[eleMu[0].ele0Ind].lv.Py()*ele[eleMu[0].ele0Ind].lv.Py()+ele[eleMu[0].ele0Ind].lv.Pz()*ele[eleMu[0].ele0Ind].lv.Pz())
+   + muo[eleMu[0].mu0Ind].lv.Py()/sqrt(muo[eleMu[0].mu0Ind].lv.Px()*muo[eleMu[0].mu0Ind].lv.Px()+ muo[eleMu[0].mu0Ind].lv.Py()*muo[eleMu[0].mu0Ind].lv.Py()+ muo[eleMu[0].mu0Ind].lv.Pz()*muo[eleMu[0].mu0Ind].lv.Pz()));
+
+     float ZetaZ=(1/sqrt(2))* (ele[eleMu[0].ele0Ind].lv.Pz() /sqrt(ele[eleMu[0].ele0Ind].lv.Px()*ele[eleMu[0].ele0Ind].lv.Px()+ele[eleMu[0].ele0Ind].lv.Py()*ele[eleMu[0].ele0Ind].lv.Py()+ ele[eleMu[0].ele0Ind].lv.Pz()*ele[eleMu[0].ele0Ind].lv.Pz())
+   + muo[eleMu[0].mu0Ind].lv.Pz()/sqrt(muo[eleMu[0].mu0Ind].lv.Px()*muo[eleMu[0].mu0Ind].lv.Px()+ muo[eleMu[0].mu0Ind].lv.Py()*muo[eleMu[0].mu0Ind].lv.Py()+ muo[eleMu[0].mu0Ind].lv.Pz()*muo[eleMu[0].mu0Ind].lv.Pz()));
+
+      float Pvisible_dot_Zeta = PvisibleX * ZetaX +  PvisibleY * ZetaY+ PvisibleZ * ZetaZ;
+      float P_dot_Zeta_met =  Pvisible_dot_Zeta + (pfmet[0].Px()*ZetaX + pfmet[0].Py()*ZetaY + pfmet[0].Py()*ZetaY ) ;
+      
+      cout<<"Pvisible_dot_Zeta1" << Pvisible_dot_Zeta <<endl;
+    
+    //if(fVerbose > 3) 
+    cout<<" met.Pt() "<<eleMu[0].lv.Pt()<<endl;
     eleMu[0].MT2Imbalanced = CalcMT2(0, 0,muo[eleMu[0].mu0Ind].lv,ele[eleMu[0].ele0Ind].lv, (-eleMu[0].lv));
     if(fVerbose > 3) cout<<"  eleMu[0].MT2Imbalanced  "<<eleMu[0].MT2Imbalanced <<endl;
-  } else{
+  } 
+     else{
     eleMu[0].mu0Ind=-1;
     eleMu[0].ele0Ind = -1;
     for(int i=0; i<NMuons; i++)
@@ -94,15 +114,21 @@ void MT2tree::FillEleMu(){
      
      float PvisibleX = ele[eleMu[0].ele0Ind].lv.Px() + muo[eleMu[0].mu0Ind].lv.Px();
      float PvisibleY = ele[eleMu[0].ele0Ind].lv.Py() + muo[eleMu[0].mu0Ind].lv.Py();
-     
-     float ZetaX=(1/sqrt(2))* (ele[eleMu[0].ele0Ind].lv.Px() /sqrt(ele[eleMu[0].ele0Ind].lv.Px()*ele[eleMu[0].ele0Ind].lv.Px()+ele[eleMu[0].ele0Ind].lv.Py()*ele[eleMu[0].ele0Ind].lv.Py()) + muo[eleMu[0].mu0Ind].lv.Px()/ sqrt(muo[eleMu[0].mu0Ind].lv.Px()*muo[eleMu[0].mu0Ind].lv.Px()+muo[eleMu[0].mu0Ind].lv.Py()*muo[eleMu[0].mu0Ind].lv.Py()));
+     float PvisibleZ = ele[eleMu[0].ele0Ind].lv.Pz() + muo[eleMu[0].mu0Ind].lv.Pz();
+ 
+     float ZetaX= (1/sqrt(2))* (ele[eleMu[0].ele0Ind].lv.Px() /sqrt(ele[eleMu[0].ele0Ind].lv.Px()*ele[eleMu[0].ele0Ind].lv.Px()+ele[eleMu[0].ele0Ind].lv.Py()*ele[eleMu[0].ele0Ind].lv.Py()+ele[eleMu[0].ele0Ind].lv.Pz()*ele[eleMu[0].ele0Ind].lv.Pz()) + muo[eleMu[0].mu0Ind].lv.Px()/ sqrt(muo[eleMu[0].mu0Ind].lv.Px()*muo[eleMu[0].mu0Ind].lv.Px()+muo[eleMu[0].mu0Ind].lv.Py()*muo[eleMu[0].mu0Ind].lv.Py()+ muo[eleMu[0].mu0Ind].lv.Pz()*muo[eleMu[0].mu0Ind].lv.Pz() ));
        
-     float ZetaY=(1/sqrt(2))* (ele[eleMu[0].ele0Ind].lv.Py() /sqrt(ele[eleMu[0].ele0Ind].lv.Px()*ele[eleMu[0].ele0Ind].lv.Px()+ele[eleMu[0].ele0Ind].lv.Py()*ele[eleMu[0].ele0Ind].lv.Py())
-   + muo[eleMu[0].mu0Ind].lv.Py()/sqrt(muo[eleMu[0].mu0Ind].lv.Px()*muo[eleMu[0].mu0Ind].lv.Px()+ muo[eleMu[0].mu0Ind].lv.Py()*muo[eleMu[0].mu0Ind].lv.Py()));
+     float ZetaY=(1/sqrt(2))* (ele[eleMu[0].ele0Ind].lv.Py() /sqrt(ele[eleMu[0].ele0Ind].lv.Px()*ele[eleMu[0].ele0Ind].lv.Px()+ele[eleMu[0].ele0Ind].lv.Py()*ele[eleMu[0].ele0Ind].lv.Py()+ele[eleMu[0].ele0Ind].lv.Pz()*ele[eleMu[0].ele0Ind].lv.Pz())
+   + muo[eleMu[0].mu0Ind].lv.Py()/sqrt(muo[eleMu[0].mu0Ind].lv.Px()*muo[eleMu[0].mu0Ind].lv.Px()+ muo[eleMu[0].mu0Ind].lv.Py()*muo[eleMu[0].mu0Ind].lv.Py()+ muo[eleMu[0].mu0Ind].lv.Pz()*muo[eleMu[0].mu0Ind].lv.Pz()));
 
-      float Pvisible_dot_Zeta = PvisibleX * ZetaX +  PvisibleY * ZetaY;
-      float Pvisible_dot_Zeta_met = PvisibleX * ZetaX +  PvisibleY * ZetaY + (pfmet[0].Pt()*(ZetaX +ZetaY)) ;
+     float ZetaZ=(1/sqrt(2))* (ele[eleMu[0].ele0Ind].lv.Pz() /sqrt(ele[eleMu[0].ele0Ind].lv.Px()*ele[eleMu[0].ele0Ind].lv.Px()+ele[eleMu[0].ele0Ind].lv.Py()*ele[eleMu[0].ele0Ind].lv.Py()+ ele[eleMu[0].ele0Ind].lv.Pz()*ele[eleMu[0].ele0Ind].lv.Pz())
+   + muo[eleMu[0].mu0Ind].lv.Pz()/sqrt(muo[eleMu[0].mu0Ind].lv.Px()*muo[eleMu[0].mu0Ind].lv.Px()+ muo[eleMu[0].mu0Ind].lv.Py()*muo[eleMu[0].mu0Ind].lv.Py()+ muo[eleMu[0].mu0Ind].lv.Pz()*muo[eleMu[0].mu0Ind].lv.Pz()));
+
+      float Pvisible_dot_Zeta = PvisibleX * ZetaX +  PvisibleY * ZetaY+ PvisibleZ * ZetaZ;
+      float P_dot_Zeta_met =  Pvisible_dot_Zeta + pfmet[0].Px()*ZetaX + pfmet[0].Py()*ZetaY + pfmet[0].Py()*ZetaY  ;
+     
       //cout<<"met.Pt() "<<met.Pt()<<endl;
+     
       eleMu[0].MT2Imbalanced = CalcMT2(0, 0,muo[eleMu[0].mu0Ind].lv,ele[eleMu[0].ele0Ind].lv, -(eleMu[0].lv));
       if(fVerbose > 3) cout<<"  eleMu[0].MT2Imbalanced  "<<eleMu[0].MT2Imbalanced <<endl;
     }
