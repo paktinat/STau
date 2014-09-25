ee33e1fa732520793698c42b45d181a8f1b7a786
a-fahim <a.fahim@gmail.com>
2014-09-25 03:51:31 +0330

    Directory setUpperLimit is added to produce exclusion limit.

---
+++ MT2Analysis/Code/MT2AnalysisCode/MT2Code/setUpperLimit/run_setUpperLimit.C
@@ -0,0 +1,148 @@
+
+#include <TString.h>
+
+void makeCard(double S, double dS, double B, double dB, string sOut) {
+
+    // === DATA CARD ===
+    ofstream fOut(sOut.c_str());
+    fOut.precision(3);
+    fOut << "imax 1  number of channels" << std::endl;
+    fOut << "jmax 1  number of backgrounds" << std::endl;
+    fOut << "kmax 2  number of nuisance parameters (sources of systematic uncertainties)" << std::endl;
+    fOut << "---" << std::endl;
+    fOut << "bin 1" << std::endl;
+    fOut << "observation " << B << std::endl;
+    fOut << "---" << std::endl;
+    fOut << "bin              1     1" << std::endl;
+    fOut << "process         SMS    All" << std::endl;
+    fOut << "process          0     1  " << std::endl;
+    fOut << "rate           " << S << "\t" << B << std::endl;
+    fOut << "---" << std::endl;
+    fOut << "dS  lnN    " << 1 + dS << "\t-" << std::endl;
+    fOut << "dB  lnN    - \t " << 1 + dB << std::endl;
+    fOut.close();
+
+}
+
+bool isOUT(double x, double y) {
+
+    if (x < 120.) return true;
+    if (x > 520.) return true;
+
+    double a = (100. - 480.) / (120. - 500.);
+    double b = -a * 120. + 100.;
+    if (y > a * x + b) return true;
+
+    a = (80. - 0.) / (120. - 200.);
+    b = -a * 120. + 80.;
+    if (y < a * x + b) return true;
+
+    return false;
+}
+
+run_setUpperLimit() {
+
+    std::vector< TString > sin;
+    int argc = gApplication->Argc();
+    for (int i = 0; i < argc; i++) {
+        TString argvi = gApplication->Argv(i);
+        //        if (argvi == "run_setUpperLimit.C")
+        if (argvi.Contains("run_setUpperLimit.C"))
+            for (int j = i + 1; j < argc; j++) {
+                TString argvj = gApplication->Argv(j);
+                sin.push_back(argvj);
+                i++;
+            }
+    }
+
+    TH2D* htmp = (TH2D*) TFile::Open("referenceXSecs.root")->Get("C1C1_8TeV_NLONLL_LSP");
+    TFile* fout = new TFile("upperLimit.root", "RECREATE");
+    fout->cd();
+
+    htmp->Reset();
+    TH2D* hObs = (TH2D*) htmp->Clone("hObs");
+    TH2D* hMdn = (TH2D*) htmp->Clone("hMdn");
+    TH2D* hSgmP1 = (TH2D*) htmp->Clone("hSgmP1");
+    TH2D* hSgmM1 = (TH2D*) htmp->Clone("hSgmM1");
+    TH2D* hSgmP2 = (TH2D*) htmp->Clone("hSgmP2");
+    TH2D* hSgmM2 = (TH2D*) htmp->Clone("hSgmM2");
+
+    TFile* fin;
+    for (int ix = 1; ix <= htmp->GetXaxis()->GetNbins(); ix++) {
+        for (int iy = 1; iy <= htmp->GetYaxis()->GetNbins(); iy++) {
+
+            if (isOUT(20 * ix, 20 * iy)) continue;
+
+            cout << "\n ============================================" << endl;
+            for (int isin = 0; isin < sin.size(); isin++) {
+                cout << ">> " << 20 * ix << "," << 20 * iy << ": " << sin[isin] << endl;
+
+                fin = new TFile(sin[isin]);
+
+                TH1* hBkg = fin->GetObjectChecked("h_PN_Bkg", "TH1");
+                Double_t B = hBkg->GetBinContent(1);
+                Double_t dB = 0.10;
+
+                TH2* hSgn = fin->GetObjectChecked("h_PN_MLSP_MChi", "TH2*");
+                Double_t S = hSgn->GetBinContent(ix, iy);
+                Double_t dS = 0.10;
+
+                fin->Close("R");
+
+                cout << "B = " << B << "\tS = " << S << endl;
+                if (S == 0) continue;
+
+                stringstream ss;
+                ss << "datacard_" << isin;
+                makeCard(S, dS, B, dB, ss.str());
+            }//isin
+
+            if (!(std::ifstream("datacard_0")).good()) continue;
+            system("combineCards.py datacard_* > datacard");
+            system("/bin/rm -f datacard_*");
+            system("combine -M Asymptotic datacard");
+
+            TTree* tree;
+            TFile * flimit = new TFile("higgsCombineTest.Asymptotic.mH120.root");
+            flimit->GetObject("limit", tree);
+
+            Double_t limit;
+            TBranch *b_limit; //!
+            tree->SetBranchAddress("limit", &limit, &b_limit);
+
+            Float_t quantileExpected;
+            TBranch *b_quantileExpected; //!
+            tree->SetBranchAddress("quantileExpected", &quantileExpected, &b_quantileExpected);
+
+            std::vector<double> vLimit;
+            Long64_t nEntrs = tree->GetEntriesFast();
+            for (Long64_t iEntr = 0; iEntr < nEntrs; iEntr++) {
+                tree->GetEntry(iEntr);
+                cout << ">> quantileExpected: " << quantileExpected << "\tlimit: " << limit << endl;
+                vLimit.push_back(limit);
+            }
+
+            double SgmP2(vLimit[0]), SgmP1(vLimit[1]), Mdn(vLimit[2]), SgmM1(vLimit[3]), SgmM2(vLimit[4]), Obs(vLimit[5]);
+            hObs->SetBinContent(ix, iy, Obs);
+            hMdn->SetBinContent(ix, iy, Mdn);
+            hSgmP1->SetBinContent(ix, iy, SgmP1);
+            hSgmM1->SetBinContent(ix, iy, SgmM1);
+            hSgmP2->SetBinContent(ix, iy, SgmP2);
+            hSgmM2->SetBinContent(ix, iy, SgmM2);
+
+            if ((std::ifstream("higgsCombineTest.Asymptotic.mH120.root")).good()) system("rm higgsCombineTest.Asymptotic.mH120.root");
+            if ((std::ifstream("datacard")).good()) system("rm datacard");
+
+        }//iy
+    }//ix
+
+    fout->Write();
+    fout->Close();
+
+    //    cout << "give a char ... " ; 
+    //    getchar();
+    //    system("/bin/ls  -a1lG");
+
+}
+
+
